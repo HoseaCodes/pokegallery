9a6c7ebeacf38d59b8f9d7d4f6e1dec2d3c81bf6
hoseacodes <dominique11h@yahoo.com>
2021-06-20 22:26:17 -0500

    feat: added types

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ $(function(){
     const $imgEl = $('.modal-content img');
     const $name = $('#name');
     const $moves = $('#moves');
+    const $type1 = $('#type1');
+    const $type2 = $('#type2');
     const $abilities = $('#abilities');
     const $height = $('#height');
     const $modal = $('.modal');
@@ -34,6 +36,7 @@ $(function(){
                 render(); // programatically render the html
             } else {
                 pokemonDetail = data;
+                console.log(pokemonDetail)
                 render(true);
             }
         }, function(error){
@@ -61,6 +64,75 @@ $(function(){
             $imgEl.attr('src', pokemonDetail.sprites.front_default);
             $imgEl.attr('alt', pokemonDetail.name);
             $name.text(pokemonDetail.name);
+            switch(pokemonDetail.types[0].type.name) {
+                case 'poison':
+                $type1[0].style.background  = 'purple'
+                $type1[0].style.color  = 'white'
+                 break;
+                case 'grass':
+                 $type1[0].style.background  = 'green'
+                $type1[0].style.color  = 'white'
+                break;
+                case 'bug':
+                 $type1[0].style.background  = 'green'
+                $type1[0].style.color  = 'white'
+                break;
+                case 'normal':
+                 $type1[0].style.background  = 'gray'
+                $type1[0].style.color  = 'white'
+                break;
+                case 'fire':
+                 $type1[0].style.background  = 'red'
+                $type1[0].style.color  = 'white'
+                break;
+                case 'water':
+                 $type1[0].style.background  = 'blue'
+                $type1[0].style.color  = 'white'
+                break;
+                case 'flying':
+                 $type1[0].style.background  = 'lightblue'
+                $type1[0].style.color  = 'white'
+                 break;
+                default:
+                null
+               }  
+            if (pokemonDetail.types.length > 1) {
+                switch(pokemonDetail.types[1].type.name) {
+                    case 'poison':
+                        $type2[0].style.background  = 'purple'
+                        $type2[0].style.color  = 'white'
+                        break;
+                    case 'grass':
+                        $type2[0].style.background  = 'green'
+                        $type2[0].style.color  = 'white'
+                        break;
+                    case 'bug':
+                        $type2[0].style.background  = 'green'
+                        $type2[0].style.color  = 'white'
+                        break;
+                    case 'normal':
+                        $type2[0].style.background  = 'gray'
+                        $type2[0].style.color  = 'white'
+                        break;
+                    case 'fire':
+                        $type2[0].style.background  = 'red'
+                        $type2[0].style.color  = 'white'
+                        break;
+                    case 'water':
+                        $type2[0].style.background  = 'blue'
+                        $type2[0].style.color  = 'white'
+                        break;
+                    case 'flying':
+                        $type2[0].style.background  = 'lightblue'
+                        $type2[0].style.color  = 'white'
+                        break;
+                    default:
+                        null
+                }  
+            }
+            $type1.text(pokemonDetail.types[0].type.name);
+            console.log(pokemonDetail.types.length)
+            pokemonDetail.types.length === 1 || undefined ? console.log(pokemonDetail.types.length) : $type2.text(pokemonDetail.types[1].type.name)    
             $height.text("Height: " + pokemonDetail.height);
             $moves.text("Number of moves: " + pokemonDetail.moves.length);
             $abilities.text("Number of abilities: " + pokemonDetail.abilities.length);
